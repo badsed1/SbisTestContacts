6ec8d2de9262b13f00645c7845d3962e6946c7bd
Женя Стадник <E.Stadnik@btcgroup.ru>
2018-06-01 10:10:54 +0300

    Initial Commit

---
+++ SbisTestContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SbisTestContacts
+//
+//  Created by Евгений Стадник on 01.06.2018.
+//  Copyright © 2018 Евгений Стадник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
